ff4cbce50e571fac9fbedc7006dc0da2bbd1371d
wz-billings <wz.billings@gmail.com>
2019-04-28 17:34:47 -0400

    Added documentation for data. RScript slightly edited to reflect.

--- Analysis/Project3Script.R
+++ Analysis/Project3Script.R
@@ -3,29 +3,7 @@
 # Hoops Billings     #
 ######################
 #This script is used to perform a multiple linear regression analysis on the USDA
-#Feed grains database cleaned data, with farmer recieved price as the response variable.
-#Data names are easily understandable in .csv file, will be changed here for convinience.
-#price = Price recieved by farmers in current market year in $/bushel
-#acreage = planted acreage of corn in market year, in millions of acres
-#harvest = millions of acres of corn harvested for grain in current MY
-#production = total production of corn in millions of bushels in current MY
-#yield = yield of corn per harvested acre, in bushels/acre for current MY
-#loan = loan rate of corn in current MY in $/bushel
-#bf = broiler:feed ratio, (NEEDS EXPLANATION)
-#ef = egg:feed ratio,
-#hc = hog:corn ratio,
-#mf = milk:feed ratio,
-#shc = steer-heifer:corn ratio,
-#tf = turkey:feed ratio,
-#heat = MY Mean global heat anomaly estimated by NASA.
-#export = 1000s of bushels of corn exported from the US in current MY.
-#import = 1000s of bushels of corn imported into the US in current MY.
-#railrate = rail rate price index, relative price of railway shipping, in current MY.
-#railcar = 1000s of rail cars loaded with grain in current MY.
-#supply = total supply of corn in US for current MY in millions of bushels.
-#disapp = total disappearance of corn in US for current MY in millions of bushels.
-#feed = amount of corn feed to livestock in current MY, in 1000s of metric tons.
-#fai = total feed animal index, relative number of GCAUs (Grain consuming animal units).
+#Feed grains database cleaned data, with farmer received price as the response variable.
 
 #Start by setting working directory, random seed, and loading needed packages
 setwd("/Users/zanebillings/Documents/Academic/2019 Spring/MATH 375/Project3/WorkingDirectory")
@@ -34,10 +12,6 @@ library(tidyverse)
 
 #import data, rename columns, create dummy var for tracking time
 CornFull <- read_csv("Feed_Grains_CornData_Clean.csv")
-colnames(CornFull) <- c("year","price","acreage","harvest","production",
-                   "yield","loan","bf","ef","hc","mf","shc","tf",
-                   "heat","export","import","railrate","railcar","supply",
-                   "disapp","feed","fai")
 n <- nrow(CornFull)
 CornFull$index <- seq(from = 0, to = (n - 1), by = 1)
 
@@ -63,7 +37,7 @@ summary(Model1866All)
 
 #Stepwise Regression for the model using the 1866 parameters
 ##Forward stepwise regression for the 1866 model
-Model1866ForwardStep <- 
+Model1866ForwardStep <-
   step(object = ModelFullReduced,
        scope = price~index+harvest+production+yield,
        data = CornTraining,
@@ -90,7 +64,7 @@ influence.measures(Model1866All)
 # colnames(Corn1926) = c("year","price","acreage","harvest","yield","heat",
 #                        "production")
 # Corn1926$index = seq(from = 0, to = nrow(Corn1926) - 1, by = 1)
-# 
+#
 # #Set up 1926 full model
 # Model1926Full = lm(formula = price~index+acreage+harvest+yield+heat+production,
 #                    data = Corn1926)
