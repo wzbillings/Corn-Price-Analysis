0046de46dc77d899c6bbc5e1ab1f65eca8cbc6f5
Zane Billings <39701545+wz-billings@users.noreply.github.com>
2021-04-17 00:05:38 -0400

    feat: created script to download data. Fixes #1. Added a script to download the feed grains data from the USDA website. Unfortunately due to website design, the link to the file needs to be manually updated every time the online dataset is updated--this is not ideal.

---
+++ Code/Raw-Data-Downloading.R
@@ -0,0 +1,25 @@
+###
+# Raw data downloading script
+# Zane Billings
+# This script downloads the current version of the USDA feed grains database.
+#  Unfortunately the USDA changes this link for every version of the database
+#  and it is AFAIK impossible to automate without scraping the webpage which
+#  seems like an awful lot of trouble.
+# So if you want to update the data you need to check
+#  https://www.ers.usda.gov/data-products/feed-grains-database/feed-grains-yearbook-tables.aspx
+#  to get the updated link.
+###
+
+library(here)
+
+# Current path as of 2021-04-16.
+pth = "https://www.ers.usda.gov/webdocs/DataFiles/50048/Feed%20Grains%20Yearbook%20Tables-All%20Years.xls?v=4138.8"
+
+# Output file name
+out = here::here("Data", "Raw-Data", "FeedGrainsAllYears.xls")
+
+# Download excel file to raw data repo.
+download.file(
+  url = pth,
+  destfile = out
+)
